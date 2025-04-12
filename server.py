e440c4422f0a5a9172bbc094e0e311246835f908
Bhavani Ravi <bhava0895@gmail.com>
2025-04-12 12:42:18 +0530

    chore: add weather server

--- server.py
+++ server.py
@@ -11,6 +11,19 @@ def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
+@mcp.tool()
+def subtract(a: int, b: int) -> int:
+    """Subtract two numbers"""
+    return a - b
+
+@mcp.tool()
+def get_latest_messages_from_slack():
+    """
+    Returns message from slack
+    """
+    return ["No new messages"]
+
+
 if __name__ == "__main__":
     print ("running mcp demo server")
     mcp.run()
